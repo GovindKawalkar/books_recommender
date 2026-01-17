83a77b406de8d27066ab8133fe935bad6b370b53
Govind Kawalkar <kawalkarfamily24@gmail.com>
2026-01-17 12:50:55 +0530

    Initialize books_recommender directory structure

---
+++ __init__.py
@@ -0,0 +1,18 @@
+books_recommender/
+│── app.py
+│── requirements.txt
+│── templates/
+│── books_recommender/
+│   ├── __init__.py
+│   ├── logger/
+│   │   ├── __init__.py
+│   │   └── log.py
+│   ├── config/
+│   │   ├── __init__.py
+│   │   └── configuration.py
+│   ├── pipeline/
+│   │   ├── __init__.py
+│   │   └── training_pipeline.py
+│   └── exception/
+│       ├── __init__.py
+│       └── exception_handler.py
